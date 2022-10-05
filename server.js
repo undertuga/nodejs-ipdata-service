1ce077b46785b79fa741ffc59c6d5c80f8378d26
Teófilo Couto <teofilo.couto@onrising.com>
2022-10-05 18:56:05 +0100

    11st test tryouts...

---
+++ server.js
@@ -0,0 +1,11 @@
+const http = require('http'),
+      config = require('./config.json')
+
+
+let requestHandler = (request, response) => {
+  response.writeHead(200);
+  response.end(request.socket.remoteAddress);
+}
+
+const server = http.createServer(requestHandler);
+server.listen(config.port);
